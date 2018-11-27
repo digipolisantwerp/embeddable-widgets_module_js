6a5cc14c2a7d3eefdebfd61c8200dc5b6c7faeda
Joeri Sebrechts <joeri.sebrechts@digipolis.be>
2018-11-27 16:05:11 +0100

    load & define widgets in framework

--- src/widgets.js
+++ src/widgets.js
@@ -1,21 +1,77 @@
-// eslint-disable-next-line no-unused-vars
-import zoid from 'zoid';
+import { Promise } from 'es6-promise';
+import zoid from 'zoid/dist/zoid.frame';
 
 // registered widgets
 const widgets = {};
+// maps url to promise or widget
+const fetchedUrls = {};
 
-function define(identifier, options) {
-  if (widgets[identifier]) {
-    throw new Error(`"${identifier}" was defined previously`);
+function xhrGet(url) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('get', url);
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          resolve(xhr.responseText);
+        } else {
+          reject(new Error(xhr.statusText));
+        }
+      }
+    };
+    xhr.send();
+  });
+}
+
+function isDefined(tag) {
+  return !!widgets[tag];
+}
+
+function define(definition) {
+  if (!definition.tag) {
+    throw new Error('unable to define a widget without a tag');
+  }
+  const tag = definition.tag;
+  if (widgets[tag]) {
+    // zoid does not support defining a component with the same tag multiple times
+    throw new Error(`"${tag}" was defined previously`);
   } else {
     // TODO: apply sane defaults (defaultLogLevel, tag, prerenderTemplate, ...)
-    widgets[identifier] = zoid.create(options);
-    return widgets[identifier];
+    widgets[tag] = zoid.create(definition);
+    return widgets[tag];
   }
+}
+
+/**
+ * Load a widget definition from a url
+ * @param {string} url The URL hosting the widget's JSON
+ * @param {object} overrides Overrides to apply to the JSON prior to defining the widget
+ * @param {boolean} force Force loading even if already loaded.
+ *                        Use only if you know what you're doing.
+ */
+function load(url, overrides, force) {
+  const loaded = fetchedUrls[url];
+  // don't load if already loading or loaded, unless forced
+  if (!loaded || force) {
+    // start loading and cache the promise
+    fetchedUrls[url] = xhrGet(url).then((response) => {
+      // TODO: normalize URL
+      const options = Object.assign(JSON.parse(response), overrides, { originalUrl: url });
+      const definition = define(options);
+      fetchedUrls[url] = definition;
+      return definition;
+    });
+  }
+  return fetchedUrls[url];
 }
 
 function render(identifier, options, elem) {
   return widgets[identifier].render(options, elem);
 }
 
-export { define, render };
+export {
+  define,
+  isDefined,
+  load,
+  render,
+};
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,21 +1,91 @@
 // @ts-nocheck
 import chai from 'chai';
+import sinon from 'sinon';
 import * as widgets from '../lib/aui-embedded-widgets';
+
+require('mocha-sinon');
 
 const expect = chai.expect;
 
+function randomTag(prefix) {
+  return prefix + Math.random().toString(36).substring(7);
+}
+
 describe('aui-embedded-widgets', () => {
   it('defines and renders', () => {
-    const definition = widgets.define('MyTestWidget', {
-      tag: 'my-test-widget',
+    const tag = randomTag('my-test-widget1');
+    const definition = widgets.define({
+      tag,
       url: 'http://example.com',
       defaultLogLevel: 'error',
     });
     expect(definition).to.be.an('object');
     const elem = document.createElement('div');
     expect(elem.innerHTML).not.to.include('<iframe');
-    const widget = widgets.render('MyTestWidget', {}, elem);
+    const widget = widgets.render(tag, {}, elem);
     expect(elem.innerHTML).to.include('<iframe');
     expect(widget).to.be.an('object');
+  });
+
+  describe('load', () => {
+    let requests = [];
+    let xhrMock = null;
+
+    beforeEach(() => {
+      xhrMock = sinon.useFakeXMLHttpRequest();
+      xhrMock.onCreate = (xhr) => {
+        requests.push(xhr);
+      };
+      requests = [];
+    });
+    afterEach(() => {
+      xhrMock.restore();
+    });
+
+    it('loads and renders', (done) => {
+      const tag = randomTag('my-test-widget2');
+      const fixture = {
+        tag,
+        url: 'http://example.com',
+        defaultLogLevel: 'error',
+      };
+      widgets
+        .load('http://example.com/widget.json', null, true) // force loading
+        .then((definition) => {
+          expect(definition).to.be.an('object');
+          expect(definition.tag).to.equal(tag);
+          expect(definition.url).to.equal(fixture.url);
+          expect(definition.defaultLogLevel).to.equal(fixture.defaultLogLevel);
+          const elem = document.createElement('div');
+          expect(elem.innerHTML).not.to.include('<iframe');
+          const widget = widgets.render(tag, {}, elem);
+          expect(elem.innerHTML).to.include('<iframe');
+          expect(widget).to.be.an('object');
+          done();
+        })
+        .catch(err => done(err));
+      // respond to xhr to kickstart .then handler
+      expect(requests.length).to.equal(1);
+      requests[0].respond(200, { 'Content-Type': 'application/json' }, JSON.stringify(fixture));
+    });
+
+    it('only loads once', (done) => {
+      const tag = randomTag('my-test-widget3');
+      const fixture = {
+        tag,
+        url: 'http://example.com',
+      };
+      const uniqueUrl = randomTag('http://example.com/json/widget3');
+      Promise.all([widgets.load(uniqueUrl), widgets.load(uniqueUrl)]).then((results) => {
+        expect(results.length).to.equal(2);
+        expect(results[0]).to.equal(results[1]);
+        expect(results[0]).to.be.an('object');
+        expect(results[0].tag).to.equal(tag);
+        done();
+      });
+      // respond to xhr to kickstart .then handler
+      expect(requests.length).to.equal(1);
+      requests[0].respond(200, { 'Content-Type': 'application/json' }, JSON.stringify(fixture));
+    });
   });
 });
